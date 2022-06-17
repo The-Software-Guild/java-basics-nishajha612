08a65780bd0f3f44bff54ab65b4f04630bae1bde
nishajha612 <105644740+nishajha612@users.noreply.github.com>
2022-06-17 11:10:05 +0100

    Add files via upload

---
+++ Dog_prog.java
@@ -0,0 +1,84 @@
+package C242;
+
+
+import java.util.*;
+
+public class Dog_prog{
+
+    public static void main(String []args){
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your dog's name?");
+
+// Entering name of dog
+
+        String name = sc.nextLine();
+
+
+
+        System.out.println("Well then, I have this highly reliable report on " + name + "'s prestigious background right here.");
+
+
+
+        System.out.println("\n\nSir " + name + " is : \n\n");
+
+
+
+//Generating random numbers
+
+        Random rand = new Random();
+
+        int sum = 0;
+
+        int a = 0;
+
+        int b = 0;
+
+        int c = 0;
+
+        int d = 0;
+
+        int e = 0;
+
+        while(sum != 100)
+
+        {
+
+            a = rand.nextInt(100);   //for first random number
+            //System.out.println(a); not use
+            b = rand.nextInt(100-a);  //2nd random number
+
+            c = rand.nextInt(100-b);  //3rd random number
+
+            d = rand.nextInt(100-c);  //4th random number
+
+            e = rand.nextInt(100-d);   //5th random number
+
+            sum = a + b+ c + d + e;   //the total number
+
+        }
+
+
+
+        System.out.println(a + "% St. Bernard");
+
+        System.out.println(b + "% Chihuahua");
+
+        System.out.println(c + "% Dramatic RedNosed Asian Pug");
+
+        System.out.println(d + "% Common Cur");
+
+        System.out.println(e + "% King Doberman");
+
+
+
+        System.out.println("\n\nWow, that's QUITE the dog!");
+
+
+
+    }
+
+}
